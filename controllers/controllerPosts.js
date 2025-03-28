7fbfd962482008bb6e191a2ec635f956e8329b62
AndreaRispoli97 <Andrea9715@outlook.it>
2025-03-28 16:26:17 +0100

    feat: Inizio milestone 2 con index e show

--- controllers/controllerPosts.js
+++ controllers/controllerPosts.js
@@ -1,12 +1,27 @@
-const posts = require('../posts');
+const posts = require('../data/posts');
 
 function index (req, res){
     res.json(posts);
 };
 
-function show (req, res){
-    res.send('Dettagli del dolce ' + req.params.id);
-};
+// function show (req, res){
+//     res.send('Dettagli del dolce ' + req.params.id);
+// };
+
+function show(req, res) {
+    // Ottieni l'id del post dalla richiesta
+    const id = parseInt(req.params.id);
+    // Trova il post corrispondente
+    const post = posts.find(post => post.id === id);
+  //if con condizione l'aver trovato un post
+    if (post) {
+        // Restituisci il post in formato json
+      res.json(post);
+    } else {
+        //In caso di errore
+      res.status(404).json({ message: 'Post non trovato' });
+    }
+  }
 
 function store (req, res){
     res.send('Creazione nuovo dolce');
